e9e25185d8a2f8bc08e9e015701edce1f6672ea4
Christoffer Lindow <chli1801@student.miun.se>
2020-10-01 21:17:47 +0200

    Super final

--- Persistence/JsonContext.cs
+++ Persistence/JsonContext.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
-using Persistence;
 using Model;
 
 namespace Persistence
@@ -37,23 +36,31 @@ namespace Persistence
         {
             List<Member> memberList = new List<Member>();
 
+            bool dataDirExists = Directory.Exists("./data");
 
+            if(!dataDirExists) {
+                Console.WriteLine("Creating data directory");
+                Directory.CreateDirectory("data");
+            }
             try
             {
                 StreamReader reader = new StreamReader(dataPath);
                 memberList = JsonConvert.DeserializeObject<List<Member>>(reader.ReadToEnd());
+                reader.Close();
                 return memberList;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Couldnt find data file, creating a new one");
-                using (FileStream fs = File.Create(dataPath));
-                   StreamWriter sw = new StreamWriter(dataPath);
-                   sw.Write("[]");
-                   sw.Close();
-                }
-                return new List<Member>();
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Creating file");
+                FileStream fs = File.Create(dataPath);
+                fs.Close();
+                StreamWriter sw = new StreamWriter(dataPath);
+                sw.Write("[]");
+                sw.Close();
             }
+            return new List<Member>();
+        }
 
         public List<Boat> Boats()
         {
